06d984b7b28924141e4d3700e5a20f488b64d98a
'Ryu-Yean' <'uyti0713@gmail.com'>
2020-11-12 09:53:49 +0900

    Practice 문제 1

--- src/com/javaex/practice01/Ex01.java
+++ src/com/javaex/practice01/Ex01.java
@@ -3,8 +3,15 @@ package com.javaex.practice01;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* int true;
+		 * int #_0f_workers;
+		 * int countOfLetterslnString;
+		 * int 1stLevel1;
+		 * int person#;
+		 * int _person;
+		 */
+		
+		//이미 등록되어있는 예약어, 숫자로 시작하는 변수명, 특수문자 등은 변수명으로 사용 불가
 	}
 
 }
